2949f058ce5eea07fef9c794357ce5a68aa71ba5
ZhuK <127308032+ZhuK112@users.noreply.github.com>
2023-04-18 19:42:35 +0300

    Лаба 2

---
+++ Laba 2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title> Amazing mountain </title>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ <link rel="stylesheet" href="assets/css/my-styles.css">
+</head>
+<body>
+    <!-- ******HEADER****** -->
+    <header id="header" class="header">
+    </header>
+    <main class="main content">
+    </main>
+    <footer class="footer">
+     <div class="container text-center">
+     <small class="copyright"><?= 'Designed by Me!' ?></small>
+</div>
+</footer>
+</body>
+</html>
